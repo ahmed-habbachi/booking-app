297642bdb3f920b68024d785130fbcc8464f738f
Ahmed HABBACHI <a.habbachi@case-tunisia.com>
2023-11-08 15:36:10 +0000

    chore: reaching the functions chapter

---
+++ README.md
@@ -0,0 +1,5 @@
+# Booking CLI
+
+This is just a tutorial repo where I am learning Golang or Go.
+I am following this long video: [Golang Tutorial for Beginners | Full Go Course](https://www.youtube.com/watch?v=yyUHQIec83I)  
+I've reached 57:16 [Encapsulate Logic with Functions](https://www.youtube.com/watch?v=yyUHQIec83I&t=7117s)
--- main.go
+++ main.go
@@ -1,35 +1,74 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 50
+	conferenceName := "Go Conference"
+	const conferenceTickets int = 50
 	var remainingTickets uint = 50
+	var bookings []string
 
 	fmt.Printf("Welcome to %v booking application\n", conferenceName)
 	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
 	fmt.Println("Get your tickets here to attend")
 
-	var firstName string
-	var lastName string
-	var email string
-	var userTickets uint
-	// ask the user for name
-	fmt.Println("Enter your first name")
-	fmt.Scan(&firstName)
+	for {
+		var firstName string
+		var lastName string
+		var email string
+		var userTickets uint
+		// ask the user for name
+		fmt.Println("Enter your first name")
+		fmt.Scan(&firstName)
 
-	fmt.Println("Enter your last name")
-	fmt.Scan(&lastName)
+		fmt.Println("Enter your last name")
+		fmt.Scan(&lastName)
 
-	fmt.Println("Enter your email")
-	fmt.Scan(&email)
+		fmt.Println("Enter your email")
+		fmt.Scan(&email)
 
-	fmt.Println("How many tickets you need")
-	fmt.Scan(&userTickets)
+		fmt.Println("How many tickets you need")
+		fmt.Scan(&userTickets)
 
-	remainingTickets = remainingTickets - userTickets
+		isValidName := len(firstName) >= 2 && len(lastName) >= 2
+		isValidEmail := strings.Contains(email, "@")
+		isValidTicketNumber := userTickets > 0 && userTickets <= remainingTickets
 
-	fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
-	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+		if isValidName && isValidEmail && isValidTicketNumber {
+
+			remainingTickets = remainingTickets - userTickets
+
+			bookings = append(bookings, firstName+" "+lastName)
+
+			fmt.Printf("Thank you %v %v for booking %v tickets, you will receive a confirmation email at %v.\n", firstName, lastName, userTickets, email)
+			fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+
+			firstNames := []string{}
+			for _, booking := range bookings {
+				var names = strings.Fields(booking)
+				var firstName = names[0]
+				firstNames = append(firstNames, firstName)
+			}
+
+			fmt.Printf("These are all our bookings: %v\n", firstNames)
+
+			if remainingTickets == 0 {
+				fmt.Println("Our conference is booked out!")
+				break
+			}
+		} else {
+			if !isValidName {
+				fmt.Println("First name or last name is too short")
+			}
+			if !isValidEmail {
+				fmt.Println("Email address you entere doesn't contain an @ sign")
+			}
+			if !isValidTicketNumber {
+				fmt.Printf("We only have %v tickets remaining, so you can't book %v tickets\n", remainingTickets, userTickets)
+			}
+		}
+	}
 }
